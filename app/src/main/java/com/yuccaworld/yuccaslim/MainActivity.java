2ddc8526eecbc6e5e98a14192195a4472d39c8e9
yucca <potatoseed@gmail.com>
2017-08-14 20:50:09 +0800

    Validation using Saripaar

--- app/src/main/java/com/yuccaworld/yuccaslim/MainActivity.java
+++ app/src/main/java/com/yuccaworld/yuccaslim/MainActivity.java
@@ -8,7 +8,6 @@ import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -17,14 +16,47 @@ import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import com.mobsandgeeks.saripaar.annotation.ConfirmEmail;
+import com.mobsandgeeks.saripaar.annotation.DecimalMax;
+import com.mobsandgeeks.saripaar.annotation.DecimalMin;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.Length;
+import com.mobsandgeeks.saripaar.annotation.Max;
+import com.mobsandgeeks.saripaar.annotation.Min;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Or;
 import com.yuccaworld.yuccaslim.data.SlimContract;
 import com.yuccaworld.yuccaslim.data.SlimDBHelper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppActivity {
 
     // Create a local field member of type SQLiteDatabase called mDb
     private SQLiteDatabase mDB;
     private SlimDBHelper mSlimDBHelper;
+
+    //@NotEmpty(messageResId = R.string.name_validation)
+    @Length(max = 100, min = 1, messageResId = R.string.name_length_validation)
+    private EditText name;
+
+    @DecimalMin(value = 10, sequence = 1, messageResId = R.string.min_weight_validation)
+    @Or
+    @DecimalMax(value = 300, sequence = 2, messageResId = R.string.max_weight_validation)
+    private EditText weight;
+
+    @Min(value = 1, messageResId = R.string.min_age_validation)
+    @Or
+    @Max(value = 300, messageResId = R.string.max_age_validation)
+    private EditText age;
+
+    @Email(messageResId = R.string.email_validation)
+    private EditText email;
+
+    @ConfirmEmail
+    private EditText confirmEmail;
+
+    private FloatingActionButton submit;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +64,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Create a SlimDbHelper instance, pass "this" to the constructor as context
         mSlimDBHelper = new SlimDBHelper(this);
+
+        // Init validator
+        initView();
+
 
         // Set the gender switch only male or female
         final Switch switchMale = (Switch)findViewById(R.id.switchMale);
@@ -62,32 +98,37 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // collect values from UI
-                ContentValues cv = new ContentValues();
-                EditText et = (EditText) findViewById(R.id.editTextName);
-                cv.put(SlimContract.SlimDB.COLUMN_USER_NAME, et.getText().toString().trim());
-                et = (EditText) findViewById(R.id.editTextMail);
-                cv.put(SlimContract.SlimDB.COLUMN_EMAIL, et.getText().toString().trim());
-                et = (EditText) findViewById(R.id.editTextAge);
-                Double d = Double.parseDouble(et.getText().toString().trim());
-                cv.put(SlimContract.SlimDB.COLUMN_AGE, d);
-                et = (EditText) findViewById(R.id.editTextWeight);
-                cv.put(SlimContract.SlimDB.COLUMN_WEIGHT, Double.parseDouble(et.getText().toString().trim()));
-                Switch s = (Switch) findViewById(R.id.switchMale);
-                if (s.isChecked()) {
-                    cv.put(SlimContract.SlimDB.COLUMN_GENDER, "M");
-                } else {
-                    s = (Switch) findViewById(R.id.switchFemale);
+                validator.validate();
+                if (validated) {
+                    Snackbar.make(view, "Good input", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                    // collect values from UI
+                    ContentValues cv = new ContentValues();
+                    EditText et = (EditText) findViewById(R.id.editTextName);
+                    cv.put(SlimContract.SlimDB.COLUMN_USER_NAME, et.getText().toString().trim());
+                    et = (EditText) findViewById(R.id.editTextMail);
+                    cv.put(SlimContract.SlimDB.COLUMN_EMAIL, et.getText().toString().trim());
+                    et = (EditText) findViewById(R.id.editTextAge);
+                    Double d = Double.parseDouble(et.getText().toString().trim());
+                    cv.put(SlimContract.SlimDB.COLUMN_AGE, d);
+                    et = (EditText) findViewById(R.id.editTextWeight);
+                    cv.put(SlimContract.SlimDB.COLUMN_WEIGHT, Double.parseDouble(et.getText().toString().trim()));
+                    Switch s = (Switch) findViewById(R.id.switchMale);
                     if (s.isChecked()) {
-                        cv.put(SlimContract.SlimDB.COLUMN_GENDER, "F");
+                        cv.put(SlimContract.SlimDB.COLUMN_GENDER, "M");
                     } else {
-                        cv.put(SlimContract.SlimDB.COLUMN_GENDER, "U");
+                        s = (Switch) findViewById(R.id.switchFemale);
+                        if (s.isChecked()) {
+                            cv.put(SlimContract.SlimDB.COLUMN_GENDER, "F");
+                        } else {
+                            cv.put(SlimContract.SlimDB.COLUMN_GENDER, "U");
+                        }
                     }
+                    ;
+                    insertDB(cv);
                 }
-                ;
-                insertDB(cv);
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
 
@@ -170,4 +211,16 @@ public class MainActivity extends AppCompatActivity {
             cursor.close();
         }
     }
+
+    private void initView() {
+        name = (EditText) findViewById(R.id.editTextName);
+        email = (EditText) findViewById(R.id.editTextMail);
+        confirmEmail = (EditText) findViewById(R.id.editTextConfrimEmail);
+        weight = (EditText) findViewById(R.id.editTextWeight);
+        age = (EditText) findViewById(R.id.editTextAge);
+        submit = (FloatingActionButton) findViewById(R.id.fab);
+        //  submit.setOnClickListener(this);
+    }
+
+
 }
