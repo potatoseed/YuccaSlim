08f566a82217d4cd290ded7230f5f0d624fa6ed4
yucca <potatoseed@gmail.com>
2017-09-07 09:07:11 +0800

    Fix Missing Utility Package Issue

---
+++ app/src/main/java/com/yuccaworld/yuccaslim/utilities/SlimUtils.java
@@ -0,0 +1,26 @@
+package com.yuccaworld.yuccaslim.utilities;
+
+import com.yuccaworld.yuccaslim.Model.ActivityInfo;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by Yung on 9/3/2017.
+ */
+
+public class SlimUtils {
+    public static ActivityInfo genFakeActivityInfo() {
+        ActivityInfo activityInfo = new ActivityInfo();
+        activityInfo.setActivityDescription("Dummy Activity");
+        activityInfo.setHint("Dummy Hint");
+        long now = System.currentTimeMillis();
+        activityInfo.setActivityTime(new Timestamp(now));
+        return activityInfo;
+    }
+
+    // Get activity type icon
+    public static int getActivityTypeIcon() {
+        return 0;
+    }
+
+}
